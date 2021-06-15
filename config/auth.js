0e5007cd1e29850bd6cfd74838c52fd5664eeb56
Elhamuddin Mahmoodi <elhamuddin.mahmoodi@gmail.com>
2021-06-15 13:51:12 +0530

    j

--- config/auth.js
+++ config/auth.js
@@ -1,51 +1,51 @@
 module.exports = {
-    ensureAuthenticated: function (req, res, next) {
-        if (req.isAuthenticated()) {
-            return next();
-        }
-        req.flash("error_msg", "Please log in to view that resource");
-        res.send({ message: "Authenitcated" });
-    },
-    forwardAuthenticated: function (req, res, next) {
-        // console.log("From Aut: ", req.body)
-        if (!req.isAuthenticated()) {
-            return next();
-        }
-        res.send({ message: "Not authenticated" });
-    },
+  ensureAuthenticated: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    req.flash("error_msg", "Please log in to view that resource");
+    res.send({ message: "Authenitcated" });
+  },
+  forwardAuthenticated: function (req, res, next) {
+    // console.log("From Aut: ", req.body)
+    if (!req.isAuthenticated()) {
+      return next();
+    }
+    res.send({ message: "Not authenticated" });
+  },
 };
 
 const jwt = require("jsonwebtoken");
 
-const generateToken = user => {
-    return jwt.sign(
-        {
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-        },
-        process.env.JWT_SECRET,
-        {
-            expiresIn: "30d",
-        }
-    );
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    },
+    process.env.JWT_SECRET || "lkajdsfiwe",
+    {
+      expiresIn: "30d",
+    }
+  );
 };
 
 const isAuth = (req, res, next) => {
-    const authorization = req.headers.authorization;
-    if (authorization) {
-        const token = authorization.slice(7, authorization.length); //bearer XXXXXX
-        jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
-            if (err) {
-                res.status(401).send({ message: "Invalid Token" });
-            } else {
-                req.user = decode;
-                next();
-            }
-        });
-    } else {
-        res.status(401).send({ message: "No token" });
-    }
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); //bearer XXXXXX
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        res.status(401).send({ message: "Invalid Token" });
+      } else {
+        req.user = decode;
+        next();
+      }
+    });
+  } else {
+    res.status(401).send({ message: "No token" });
+  }
 };
 
-module.exports = {generateToken, isAuth}
+module.exports = { generateToken, isAuth };
